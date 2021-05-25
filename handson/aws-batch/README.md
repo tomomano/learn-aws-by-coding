20419bd28e2b2aa8527a6babbdfea8403a24c8a5
Tomoyuki Mano <tomoyukimano@gmail.com>
2021-05-26 00:06:57 +0900

    #5 - WIP

--- handson/aws-batch/README.md
+++ handson/aws-batch/README.md
@@ -17,11 +17,26 @@ export AWS_SECRET_ACCESS_KEY=ABCDEFGHIJK
 export AWS_DEFAULT_REGION=ap-northeast-1
 ```
 
-Execute deploy:
+Run deploy:
 
 ```bash
 cdk deploy
 ```
+
+## Build and upload docker image
+
+Once the deployment is complete, go to the AWS console through your browser.
+Then, navigate to `ECR` > `Repositories`.
+There you will find a repository named `simplebatch-repositoryXXXX` (where XXXX is a random string generated for you).
+Open this repository, and on the top right corner hit the button saying `View push commands`.
+Simply execute each command displayed in the window (see picture).
+Now your docker image is ready in AWS!
+
+<img src="screenshot_ecr.png" width=400 />
+
+## Submiting a single task
+
+## 
 
 ## Destroy
 
--- handson/aws-batch/docker/main.py
+++ handson/aws-batch/docker/main.py
@@ -1,41 +1,75 @@
 import torch
 import torch.optim as optim
-import torchvision
 from torchvision import datasets, transforms
-from matplotlib import pyplot as plt
-
-# custom functions and classes
 from simple_mnist import Model, train, evaluate
 
-def main():
+import numpy as np
+import pandas as pd
+import os, glob
+import boto3
+import argparse
+
+def main(lr: float,
+         momentum: float,
+         epochs: int = 50,
+         upload_to_s3: bool = False,):
+
     transf = transforms.Compose([transforms.ToTensor(),
                                 transforms.Normalize((0.1307,), (0.3081,))])
-
-    trainvalset = datasets.MNIST(root='./data', train=True, download=True, transform=transf)
+    trainvalset = datasets.MNIST(root='/mnist', train=True, download=True, transform=transf)
 
     # Split train data into train and validation set
-    train_size = int(len(trainvalset) * 0.8)
-    val_size = len(trainvalset) - train_size
+    train_size = int(len(trainvalset) * 0.8) # use 80% as training data 
+    val_size = len(trainvalset) - train_size # and 20% as validation data
     trainset, valset = torch.utils.data.random_split(trainvalset, [train_size, val_size])
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
     valloader = torch.utils.data.DataLoader(valset, batch_size=64, shuffle=True)
 
-    testset = datasets.MNIST(root='./data', train=False, download=True, transform=transf)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=1000, shuffle=False)
-
     model = Model()
     model.to("cuda")
 
-    optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum)
 
-    train_losses = []
-    for epoch in range(5):
+    metrics = np.zeros((epochs, 3))
+    for epoch in range(epochs):
         losses = train(model, trainloader, optimizer, epoch)
-        train_losses = train_losses + losses
-        test_loss, test_accuracy = evaluate(model, testloader)
-        print(f"\nTest set: Average loss: {test_loss:.4f}, Accuracy: {test_accuracy:.1f}%\n")
+        val_loss, val_accuracy = evaluate(model, valloader)
+        print(f"\nTest set: Average loss: {val_loss:.4f}, Accuracy: {val_accuracy:.1f}%\n")
+        metrics[epoch] = [np.mean(losses), val_loss, val_accuracy]
+
+    df = pd.DataFrame(data=metrics, columns=["train_loss", "val_loss", "val_accuracy"])
+    filename = f"metrics_{lr:0.4f}_{momentum:0.4f}.csv"
+    df.to_csv(filename, header=True, index=False)
     
-    torch.save(model.state_dict(), "mnist_cnn.pt")
+    # save the result in S3
+    if upload_to_s3:
+        transfer_to_s3(filename, filename)
+
+def transfer_to_s3(src, key):
+    """
+    Given a file path, upload and save the file in S3 with the given key
+    Parameters
+    ----------
+    src: str
+        File name of the original file
+    key: str
+        Key of the file used in S3
+    """
+    print("Saving results in S3 bucket...")
+    s3 = boto3.resource("s3")
+    bucket = s3.Bucket(os.environ["BUCKET_NAME"])
+    bucket.upload_file(src, key)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--lr", type=float,
+                        help="Learning rate")
+    parser.add_argument("-m", "--momentum", type=float,
+                        help="Momentum")
+    parser.add_argument("-e", "--epochs", type=int, default=50,
+                        help="Total number of training epochs")
+    parser.add_argument("-u", "--uploadS3", type=bool, default=False,
+                        help="If true, the metric data is saved in S3")
+    args = parser.parse_args()
+
+    main(args.lr, args.momentum, args.epochs)
