34ac02d6f1319134e0de8753cf729e863aff6523
Tomoyuki Mano <tomoyukimano@gmail.com>
2021-07-06 20:28:05 +0900

    WIP

---
+++ handson/serverless/s3/simple_s3.py
@@ -0,0 +1,41 @@
+import boto3
+import argparse
+import os
+
+s3 = boto3.resource('s3')
+
+def upload_file(bucket_name, filename, key=None):
+    bucket = s3.Bucket(bucket_name)
+
+    if key is None:
+        key = os.path.basename(filename)
+
+    bucket.upload_file(filename, key)
+    print("Upload completed.")
+    print("Original file:", filename)
+    print("Key in bucket:", key)
+
+def download_file(bucket_name, key, filename=None):
+    bucket = s3.Bucket(bucket_name)
+
+    if filename is None:
+        filename = os.path.basename(key)
+
+    bucket.download_file(key, filename)
+    print("Download completed.")
+
+if __name__ == "__main__":
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("bucket_name", type=str)
+    subparsers = parser.add_subparsers(dest="command")
+    p1 = subparsers.add_parser("upload")
+    p1.add_argument("filename", type=str)
+    p2 = subparsers.add_parser("download")
+    p2.add_argument("key", type=str)
+    args = parser.parse_args()
+
+    if args.command == "upload":
+        upload_file(args.bucket_name, args.filename)
+    elif args.command == "download":
+        download_file(args.bucket_name, args.key)
