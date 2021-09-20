4d67f199dcfc1fccd7015e84e47cb49bd00eb8f3
Tomoyuki Mano <tomoyukimano@gmail.com>
2021-09-21 01:00:30 +0900

    working on the introduction to cloud computing

--- book-en/generate.sh
+++ book-en/generate.sh
@@ -4,6 +4,6 @@ echo generating html... "${file%.*}"
 bundler exec asciidoctor -o "dist/index.html" $file
 cp -r imgs dist/
 
-echo Green generating pdf by asciidoctor-pdf with thema ...
-bundler exec asciidoctor -o "dist/${file%.*}.pdf" \
--r asciidoctor-pdf -d book -a "data-uri!" $file
+# echo Green generating pdf by asciidoctor-pdf with thema ...
+# bundler exec asciidoctor -o "dist/${file%.*}.pdf" \
+# -r asciidoctor-pdf -d book -a "data-uri!" $file
